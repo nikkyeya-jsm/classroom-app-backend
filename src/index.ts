dd9da4067e116c5b846a00ba261ab8c11256b913
nikkyeya <nikkyeya@gmail.com>
2025-12-09 12:55:09 +0800

    APM site24x7 integration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Site24x7 APM Insight Initialization
+import AgentAPI from 'apminsight';
+AgentAPI.config();
+
 import express from 'express';
 import cors from 'cors';
 import { toNodeHandler } from 'better-auth/node';
